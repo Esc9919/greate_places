84ecaa2614ccf2d3284c1dd0ecfc1aa2f9717373
Esc9919 <emersoncaldeira1999@hotmail.com>
2024-10-14 20:00:18 -0300

    Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:greate_places/providers/greate_places.dart';
 import 'package:greate_places/screens/place_form_screen.dart';
 import 'package:greate_places/screens/places_list_screen.dart';
 import 'package:greate_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,21 +14,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Great Places',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.blueAccent,
-          secondary: Colors.amber,
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatePlaces(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Great Places',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.blueAccent,
+            secondary: Colors.amber,
+          ),
+          canvasColor: Colors.white,
+          useMaterial3: true,
         ),
-        canvasColor: Colors.white,
-        useMaterial3: true,
+        home: const PlacesListScreen(),
+        routes: {
+          AppRoutes.PLACE_FORM: (ctx) => const PlaceFormScreen(),
+        },
       ),
-      home: const PlacesListScreen(),
-      routes: {
-        AppRoutes.PLACE_FORM: (ctx) => const PlaceFormScreen(),
-      },
     );
   }
 }
--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:greate_places/providers/greate_places.dart';
 import 'package:greate_places/widgets/image_input.dart';
+import 'package:provider/provider.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({super.key});
@@ -9,10 +13,26 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
-
   final _titleController = TextEditingController();
 
-  void _submitForm() {}
+  File? _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _submitForm() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+
+    Provider.of<GreatePlaces>(context, listen: false).addPlace(
+      _titleController.text,
+      _pickedImage!,
+    );
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +61,7 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                       ),
                     ),
                     SizedBox(height: 10),
-                    const ImageInput(),
+                    ImageInput(_selectImage),
                   ],
                 ),
               ),
